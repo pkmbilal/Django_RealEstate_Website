22165c796051519bef87b0ac1e8185720a143e78
Mohammed <bilal00717@gmail.com>
2022-09-01 10:34:59 +0300

    Before Migration

--- webapp/urls.py
+++ webapp/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.Home, name='home'),
+    path('buildings', views.Buildings, name='buildings'),
+    path('rooms', views.Rooms, name='rooms'),
+    path('customers', views.Customers, name='customers'),
+    path('receipts', views.Receipts, name='receipts'),
+    path('new_building', views.NewBuilding, name='new_building'),
+    path('new_room', views.NewRoom, name='new_room'),
+    path('new_customer', views.NewCustomer, name='new_customer'),
+    path('new_receipt', views.NewReceipt, name='new_receipt'),
+    path('login', views.Login, name='login'),
+    path('signup', views.Signup, name='signup'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+def Home(request):
+    return render(request, 'index.html', {'context':'null'})
+
+def Buildings(request):
+    return render(request, 'buildings.html', {'context':'Buildings'})
+
+def Rooms(request):
+    return render(request, 'rooms.html', {'context':'Rooms'})
+
+def Customers(request):
+    return render(request, 'customers.html', {'context':'Customers'})
+
+def Receipts(request):
+    return render(request, 'receipts.html', {'context':'Receipts'})
+
+def NewBuilding(request):
+    return render(request, 'new_building.html', {'context':'NewBuilding'})
+
+def NewRoom(request):
+    return render(request, 'new_room.html', {'context':'NewRoom'})
+
+def NewCustomer(request):
+    return render(request, 'new_customer.html', {'context':'NewCustomer'})
+
+def NewReceipt(request):
+    return render(request, 'new_receipt.html', {'context':'NewReceipt'})
+
+def Login(request):
+    return render(request, 'login.html')
+
+def Signup(request):
+    return render(request, 'signup.html')
